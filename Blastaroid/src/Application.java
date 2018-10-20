c637607dfbeac6dd02a0595eb9c610203f61ab70
Akshay Kumar <aksh0001@student.monash.edu>
2018-10-20 18:26:44 +1100

    Added Application class - created JFrame

---
+++ Blastaroid/src/Application.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Application {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame(); // Create a new JFrame
+        jf.setTitle("Blastaroid");
+        jf.setBounds(10, 10, 700, 600);
+        jf.setResizable(false); // don't make it resizeable
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(3); // Can also make use if constant JFrame.EXIT_ON_CLOSE
+
+
+
+    }
+}
